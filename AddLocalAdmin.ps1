f9b3fdb3f8ed1db9a75862f6342e2f3f1b8444e0
Asheekujjaman <158477994+asheekujjaman@users.noreply.github.com>
2026-06-08 11:33:30 +0600

    Update AddLocalAdmin.ps1

--- AddLocalAdmin.ps1
+++ AddLocalAdmin.ps1
@@ -1,1 +1,10 @@
+# Define the username and password
+$UserName = "ashik"
+$Password = "nil.1564" | ConvertTo-SecureString -AsPlainText -Force
+
+# Create the local user
+New-LocalUser -Name $UserName -Password $Password -FullName "Local Administrator" -Description "SCCM deployed local admin account"
+
+# Add the user to Administrators group
+Add-LocalGroupMember -Group "Administrators" -Member $UserName
 
